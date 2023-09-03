1fba91bec49ab888ecd8641262abe3f919fd1370
hanna3-14 <114485436+hanna3-14@users.noreply.github.com>
2023-09-03 16:59:58 +0200

    updated low battery notifications

--- .bash-scripts/alert-battery.sh
+++ .bash-scripts/alert-battery.sh
@@ -2,8 +2,13 @@
 bat_files="/sys/class/power_supply/BAT0"
 bat_status=$(cat "${bat_files}/status")
 capacity=$(cat "${bat_files}/capacity")
-if [[ "${bat_status}" == "Discharging" && ${capacity} -le 20 ]]; then
-	notify-send \
+if [[ "${bat_status}" == "Discharging" && ${capacity} -le 10 ]]; then
+	notify-send --urgency=CRITICAL \
+		--icon=${HOME}/.config/pictures/sad.jpg \
+		"I am reeeeeeeeally hungry" \
+		"Only ${capacity}% battery remaining"
+elif [[ "${bat_status}" == "Discharging" && ${capacity} -le 20 ]]; then
+	notify-send --urgency=CRITICAL \
 		--icon=${HOME}/.config/pictures/low_battery.png \
 		"I am hungry, give me electricity" \
 		"Only ${capacity}% battery remaining"
