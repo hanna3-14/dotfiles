8e60abe2e8121d431ee42cdbe1d630885d6e7ff3
hanna3-14 <114485436+hanna3-14@users.noreply.github.com>
2025-01-16 19:54:12 +0100

    add simple terminal in neovim

--- .config/nvim/lua/config/vim-options.lua
+++ .config/nvim/lua/config/vim-options.lua
@@ -3,6 +3,10 @@ vim.cmd("vnoremap ¿ <esc>")
 vim.cmd("set number")
 vim.cmd("autocmd BufRead,BufNewFile * setlocal spell spelllang=en_us")
 
+-- open terminal
+vim.keymap.set("n", "<C-m>", ":vertical belowright terminal<CR>i")
+vim.keymap.set('t', '¿', '<C-\\><C-n>') -- keymap for exiting terminal mode
+
 
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
