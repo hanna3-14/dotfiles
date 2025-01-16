b72e4026fff656dac2d17439bd8da5cd080af5bb
hanna3-14 <114485436+hanna3-14@users.noreply.github.com>
2025-01-16 19:50:44 +0100

    enable spell checker

--- .config/nvim/lua/config/vim-options.lua
+++ .config/nvim/lua/config/vim-options.lua
@@ -1,6 +1,8 @@
 vim.cmd("inoremap ¿ <esc>")
 vim.cmd("vnoremap ¿ <esc>")
 vim.cmd("set number")
+vim.cmd("autocmd BufRead,BufNewFile * setlocal spell spelllang=en_us")
+
 
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
