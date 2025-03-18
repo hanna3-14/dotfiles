8ab5e588e7bd6c4efcb272488e91682cc7b4a233
hanna3-14 <114485436+hanna3-14@users.noreply.github.com>
2025-03-18 15:00:44 +0100

    add neotree

---
+++ .config/nvim/lua/plugins/neotree.lua
@@ -0,0 +1,22 @@
+-- https://github.com/nvim-neo-tree/neo-tree.nvim?tab=readme-ov-file#longer-example-for-lazynvim
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+  },
+  config = function()
+    require("neo-tree").setup({
+      event_handlers = {
+        {
+          event = "file_open_requested",
+          handler = function()
+            require("neo-tree.command").execute({ action = "close" })
+          end,
+        },
+      },
+    })
+    vim.keymap.set("n", "<C-n>", ":Neotree toggle reveal left<CR>", {})
+  end,
+}
